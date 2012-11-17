129fe581634f37eebc20ee4a3737ba25aa6a795b
Gregory Eric Sanderson <gzou2000@gmail.com>
2012-11-16 22:59:11 -0500

    add version filtering to CLI

--- animesubs/main.py
+++ animesubs/main.py
@@ -12,6 +12,7 @@ def main():
     parser = argparse.ArgumentParser(
         description="download torrent files from anime feeds")
     parser.add_argument("--filescan", help="scan directory for existing files before downloading torrents")
+    parser.add_argument("--filter-versions", action='store_true', help="filter anime episodes that have multiple versions (keep last version)")
     parser.add_argument("config", help="YAML config file")
     parser.add_argument("torrent_dir", help="torrent files will be downloaded here")
 
@@ -21,6 +22,9 @@ def main():
         config = yaml.load(configfile)
 
     anime = feeds.fetch_episodes_from_feeds(config)
+
+    if args.filter_versions:
+        anime = feeds.filter_episode_versions(anime)
 
     if args.filescan:
         files = [x.decode('utf8') for x in os.listdir(args.filescan)]
